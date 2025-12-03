78b3933d2dc3c355d12b489cd8dff06a91dc0dc8
Lukas Bjarre <lukas.bjarre@gmail.com>
2025-12-03 10:03:48 +0100

    Add run all mode

--- src/main.zig
+++ src/main.zig
@@ -5,34 +5,41 @@ pub fn main() !void {
     var gpa: std.heap.GeneralPurposeAllocator(.{}) = .init;
     const alloc = gpa.allocator();
 
+    var stdout_buf: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buf);
+    var stdout = &stdout_writer.interface;
+    defer stdout.flush() catch {};
+
     const args = try std.process.argsAlloc(alloc);
     defer std.process.argsFree(alloc, args);
 
     if (args.len != 2) {
-        _ = try std.fs.File.stderr().write("usage: aoc2025 DAY\n");
+        _ = try std.fs.File.stderr().write("usage: aoc2025 [DAY | 'all']\n");
         return error.IncorrectArgs;
     }
-    const day = try std.fmt.parseInt(u8, args[1], 10);
-    const input = try readInput(alloc, day);
 
-    var stdout_buf: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buf);
-    const stdout = &stdout_writer.interface;
-
-    try aoc2025.solve(stdout, input, day);
-    try stdout.flush();
+    const mode = try Mode.parse(args[1]);
+    try mode.exec(alloc, stdout);
 }
 
-fn readInput(alloc: std.mem.Allocator, day: u8) ![]const u8 {
-    var buf: [128]u8 = undefined;
+const Mode = union(enum) {
+    /// Solve all days.
+    all,
+    /// Solve a single day.
+    day: u8,
 
-    // TODO: how do I zero-pad a number in a nicer way
-    var filename: []const u8 = undefined;
-    if (day < 10) {
-        filename = try std.fmt.bufPrint(&buf, "input/day0{d}.txt", .{day});
-    } else {
-        filename = try std.fmt.bufPrint(&buf, "input/day{d}.txt", .{day});
+    fn parse(arg: []const u8) !Mode {
+        if (std.mem.eql(u8, arg, "all")) {
+            return .all;
+        }
+        const day = try std.fmt.parseInt(u8, arg, 10);
+        return .{ .day = day };
     }
 
-    return try std.fs.cwd().readFileAlloc(alloc, filename, 100 * 1024);
-}
+    fn exec(self: Mode, alloc: std.mem.Allocator, writer: *std.Io.Writer) !void {
+        try switch (self) {
+            .all => aoc2025.solveAll(alloc, writer),
+            .day => |day| aoc2025.solve(alloc, writer, day),
+        };
+    }
+};
--- src/root.zig
+++ src/root.zig
@@ -1,14 +1,35 @@
 const std = @import("std");
 
-const day01 = @import("./day01.zig");
-const day02 = @import("./day02.zig");
-const day03 = @import("./day03.zig");
+const days = 3;
 
-pub fn solve(writer: *std.Io.Writer, input: []const u8, day: u8) !void {
+pub fn solve(alloc: std.mem.Allocator, writer: *std.Io.Writer, day: u8) !void {
+    const input = try readInput(alloc, day);
+    defer alloc.free(input);
     try switch (day) {
-        1 => day01.solve(writer, input),
-        2 => day02.solve(writer, input),
-        3 => day03.solve(writer, input),
+        1 => @import("./day01.zig").solve(writer, input),
+        2 => @import("./day02.zig").solve(writer, input),
+        3 => @import("./day03.zig").solve(writer, input),
         else => error.InvalidDay,
     };
+}
+
+pub fn solveAll(alloc: std.mem.Allocator, writer: *std.Io.Writer) !void {
+    for (1..days + 1) |day| {
+        try writer.print("day {d}\n", .{day});
+        try solve(alloc, writer, @intCast(day));
+    }
+}
+
+fn readInput(alloc: std.mem.Allocator, day: u8) ![]const u8 {
+    var buf: [128]u8 = undefined;
+
+    // TODO: how do I zero-pad a number in a nicer way
+    var filename: []const u8 = undefined;
+    if (day < 10) {
+        filename = try std.fmt.bufPrint(&buf, "input/day0{d}.txt", .{day});
+    } else {
+        filename = try std.fmt.bufPrint(&buf, "input/day{d}.txt", .{day});
+    }
+
+    return try std.fs.cwd().readFileAlloc(alloc, filename, 100 * 1024);
 }
